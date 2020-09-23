92b1f33ef2bc4d6f9387f3267b0b65b0cd4591b2
Douglas <62155670+dougvalerio@users.noreply.github.com>
2020-09-22 23:22:52 -0300

    Commit Final

---
+++ js/botao-sobre.js
@@ -0,0 +1,6 @@
+var btnSobre = document.querySelector("#botao-sobre");
+
+btnSobre.addEventListener("click", function(){
+    alert("Este projeto foi feito para um processo seletivo de uma vaga de estágio da empresa Helpper. Onde tinha como objetivo fazer um Formulário de Cadastro de Clientes, com a opção de excluir os clientes. Foi utilizado HTML, CSS e JavaScript.")
+})
+
